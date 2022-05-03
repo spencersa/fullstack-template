28dd9c497f09de53fdba62209898f0b99b60447e
Samuel Spencer <spencersa715@gmail.com>
2022-05-03 16:30:40 -0500

    Initial commit

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -1,7 +1,63 @@
 <?php
-/**
- * This is a template php file for your countries search.
- * Use as you will, or start over. It's up to you.
- */
  header('Content-Type: application/json');
- echo json_encode(['data' => ['Your data']]);
+ header('Access-Control-Allow-Origin: *');
+ header('Access-Control-Allow-Methods: GET');
+
+ $queryString = $_SERVER['QUERY_STRING'];
+ parse_str($queryString, $queryStringParameters);
+
+ if ($queryStringParameters["name"]) {
+   $resultOkay = true;
+   $result = getCountriesName($queryStringParameters["name"], filter_var($queryStringParameters["full-name"], FILTER_VALIDATE_BOOLEAN));
+ }
+ else if ($queryStringParameters["country-code"]) {
+   $resultOkay = true;
+   $result = getCountriesCountryCode($queryStringParameters["country-code"]);
+ }
+
+ if ($result->statusCode == 200) {
+   echo $result->data;
+ }
+ else if ($result->statusCode == 404) {
+   header('HTTP/1.1 404 Not Found');
+ }
+ else {
+   header('HTTP/1.1 500 Oh crap');
+ }
+
+ function getCountriesName($name, $fullName)
+ {
+    $url = "https://restcountries.com/v3.1/name/$name";
+    if ($fullName && $fullName == true) {
+        $url .= "?fullText=true";
+    }
+    return apiGet($url);
+ }
+
+ function getCountriesCountryCode($countryCode)
+ {
+    $url = "https://restcountries.com/v3.1/alpha/$countryCode";
+    return apiGet($url);
+ }
+
+ class Response{
+   public $statusCode;
+   public $data;
+ }
+
+ function apiGet($url)
+ {
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    
+    $responseData = curl_exec($curl);
+    $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+    $response = new Response();
+    $response->statusCode=$http_status;
+    $response->data=$responseData;
+    return $response;
+ }
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,2 +1,35 @@
-//Write your javascript here, or roll your own. It's up to you.
-//Make your ajax call to http://localhost:8765/api/index.php here
+$(document).ready(function () {
+
+    $("#countryForm").submit(function (event) {
+        const countryName = $("#countryName").val();
+        const searchFullName = $("#searchFullName").is(':checked') ? true : false;
+        $('#resultData').remove();
+        
+        let endpoint = `http://localhost:8765/api/index.php?name=${countryName}&full-name=${searchFullName}&country-code=`
+        $.ajax({
+            url: endpoint,
+            success: function(result){
+                $('#result').append(`<div id="resultData"></div>`);
+
+                $.each(result, function(key, value){
+                    console.log(value);
+                     $('#resultData').append(`<div id="resultDataElement"> 
+                        <h3>Name: ${value.name.common}</h3>
+                        Alpha Code 2: ${value.cca2} <br>
+                        <br>
+                     </div>`);
+                });
+            },
+            error: function(result){ 
+                if (result.status == 404) {
+                   $('#result').append(`<div id="resultData">Search yielded no results <br></div>`);
+                } 
+                else {
+                   throw result;
+                }
+            }
+        })
+      event.preventDefault();
+    });
+
+  });
